44e4ae8cf0c0d98df871eab7bf62646a2a39402b
Gabriel-Q2 <quintanilhagabriel01@hotmail.com>
2026-08-06 11:05:24 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[]) {
+	
+	int a,b,c;
+	
+	printf("Manda o primeiro numero ai:\n");
+	scanf("%d",&a);
+	
+	printf("Manda o segundo numero ai:\n");
+	scanf("%d",&b);
+	
+	c = a + b;
+	
+	
+	printf("A soma de %d com %d e igual a: %d",a,b,c);
+
+	
+	return 0;
+}
+
+
+
