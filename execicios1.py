b64b7a7cc5a7c457799c9ad40474fe2190d20351
Gabriel-Q2 <quintanilhagabriel01@hotmail.com>
2026-08-18 09:01:05 -0300

    Update execicios1.py

--- execicios1.py
+++ execicios1.py
@@ -25,3 +25,15 @@ if num > num2:
     print(f"{num} e maior que {num2}\n")
 else:
     print(f"{num2} e maior que {num}\n")
+
+print("--------------------------\n")
+
+print("Agora, veremos qual um outro numero: \n")
+num3 = int(input("Digite um terceiro numero: "))
+
+if num > num2 and num > num3:
+    print("{} e maior que {} e {}".format(num, num2, num3))
+elif num2 > num and num2 > num3:
+    print("{} e maior que {} e {}".format(num2, num, num3))
+else:
+    print("{} e maior que {} e {}".format(num3, num, num2))
