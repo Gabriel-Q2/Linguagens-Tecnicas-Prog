e31b993d04a2a2f7fe6dc9694243135bdf032678
Gabriel-Q2 <quintanilhagabriel01@hotmail.com>
2026-08-19 11:12:08 -0300

    Update exec5.c

--- exercicios - C/exec5.c
+++ exercicios - C/exec5.c
@@ -2,24 +2,13 @@
 #include <stdlib.h>
 #include <math.h>
 
-
-
 int main(int argc, char *argv[]) {
 	
 	int num1, num2, num3, num4, soma, media, prod;
 	
-	printf("Digite o primeiro numero: \n");
-	scanf("%d", &num1);
-	
-	printf("Digite o segundo numero: \n");
-	scanf("%d", &num2);
-	
-	printf("Digite o terceiro numero: \n");
-	scanf("%d", &num3);
-	
-	printf("Digite o quarto numero: \n");
-	scanf("%d", &num4);	
-	
+	printf("Digite quatro numeros (com espacamento): \n");
+	scanf("%d %d %d %d", &num1, &num2, &num3, num4);
+
 	soma = num1 + num2 + num3 + num4;
 	media = (num1 + num2 + num3 + num4) / 4;
 	prod = num1 * num2 * num3 * num4;
