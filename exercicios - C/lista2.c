4eec75c392620ea99c3244be0cd719d0ec134521
Gabriel-Q2 <quintanilhagabriel01@hotmail.com>
2026-08-20 11:27:04 -0300

    Update lista2.c

--- exercicios - C/lista2.c
+++ exercicios - C/lista2.c
@@ -4,17 +4,20 @@
 
 int main(int argc, char *argv[]) {
 	
-	int a, b, c, maior, maior_temp;
+	int a, b, c, d, maior, maior_temp1, maior_temp2;
 	printf("Informe os valores a serem comparados: ");
-	scanf("%d %d %d", &a, &b, &c);
+	scanf("%d %d %d %d", &a, &b, &c, &d);
 	
 	//conta
-	maior_temp = (a+b+abs(a-b)) / 2;
+	// compara os 2 primeiros
+	maior_temp1 = (a+b+abs(a-b)) / 2;
+	//compara os dois ultimos
+	maior_temp2 = (c+d+abs(c-d)) / 2;
 	
-	maior = (maior_temp+c+abs(maior_temp-c))/2;
 	
-	printf("O maior valor entre |%d|%d|%d| = %d\n", a, b, c, maior);	
+	maior = (maior_temp1+maior_temp2+abs(maior_temp1-maior_temp2))/2;
+	
+	printf("O maior valor entre |%d|%d|%d|%d| = %d\n", a, b, c, d, maior);	
 
   	return 0;
-}	
-'
+}
