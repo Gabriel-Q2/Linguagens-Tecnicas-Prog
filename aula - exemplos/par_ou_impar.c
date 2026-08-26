5fe567b44f993d883f60da06e203299a9502603f
Gabriel-Q2 <quintanilhagabriel01@hotmail.com>
2026-08-26 11:13:08 -0300

    Update par_ou_impar.c

--- aula - exemplos/par_ou_impar.c
+++ aula - exemplos/par_ou_impar.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int a, aux;
+	
+	printf("Entre com um valor pra ver se e par ou impar\n");
+	scanf("%d", &a);
+	
+	aux = a % 2;
+	
+	if (aux == 0) {
+		printf("%d eh par.\n", a);
+	} else {
+		printf("%d eh impar.\n", a);
+	}
+
+	return 0;
+}
