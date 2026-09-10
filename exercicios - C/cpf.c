81e3e1f171cede84f6351656eb0cbdf293d0f4e2
Gabriel Q. <quintanilhagabriel01@hotmail.com>
2026-09-10 10:14:05 -0300

    Update cpf.c
    corrigido bug da validação

--- exercicios - C/cpf.c
+++ exercicios - C/cpf.c
@@ -1,65 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int validaPrimeiro(int num1, int num2, int num3, int num4, int num5,
+                   int num6, int num7, int num8, int num9) {
+    int soma, resto, valid;
 
-int validaPrimeiro(num1, num2, num3, num4, num5, num6, num7, num8, num9) {
-	int valid1, valid2, valid3, valid4, valid5, valid6, valid7, valid8, valid9, soma, resto, valid;
+    soma = num1 * 10 +
+           num2 * 9 +
+           num3 * 8 +
+           num4 * 7 +
+           num5 * 6 +
+           num6 * 5 +
+           num7 * 4 +
+           num8 * 3 +
+           num9 * 2;
+
+    resto = soma % 11;
 	
-	valid1 = num1*10;
-	valid2 = num2*9;
-	valid3 = num3*8;
-	valid4 = num4*7;
-	valid5 = num5*6;
-	valid6 = num6*5;
-	valid7 = num7*4;
-	valid8 = num8*3;
-	valid9 = num9*2;
-	
-	soma = valid1 + valid2 + valid3 + valid4 + valid5 + valid6 + valid7 + valid8 + valid9;
-	resto = (soma * 10)%11;
 	if (resto == 10) resto = 0;
-	
-	if (resto < 2) {
-		valid = 0;
-	} else {
-		valid = 11 - resto;
-	}
-	
-	return valid;
+    if (resto < 2) {
+        valid = 0;
+    } else {
+        valid = 11 - resto;
+    }
+
+    return valid;
 }
 
-int validaSegundo(num1, num2, num3, num4, num5, num6, num7, num8, num9, num10) {
-	int valid1, valid2, valid3, valid4, valid5, valid6, valid7, valid8, valid9, valid10, soma, resto, valid;
-	
-	valid1 = num1*11;
-	valid2 = num2*10;
-	valid3 = num3*9;
-	valid4 = num4*8;
-	valid5 = num5*7;
-	valid6 = num6*6;
-	valid7 = num7*5;
-	valid8 = num8*4;
-	valid9 = num9*3;
-	valid10 = num10*2;
-	
-	soma = valid1 + valid2 + valid3 + valid4 + valid5 + valid6 + valid7 + valid8 + valid9 + valid10;
-	resto = (soma * 10)%11;
-	if (resto == 10) resto = 0;
-	
-	if (resto < 2) {
-		valid = 0;
-	} else {
-		valid = 11 - resto;
-	}
-	
-	return valid;
+int validaSegundo(int num1, int num2, int num3, int num4, int num5,
+                  int num6, int num7, int num8, int num9, int num10) {
+    int soma, resto, valid;
 
+    soma = num1 * 11 +
+           num2 * 10 +
+           num3 * 9 +
+           num4 * 8 +
+           num5 * 7 +
+           num6 * 6 +
+           num7 * 5 +
+           num8 * 4 +
+           num9 * 3 +
+           num10 * 2;
+
+    resto = soma % 11;
+	
+	if (resto == 10) resto = 0;
+    if (resto < 2) {
+        valid = 0;
+    } else {
+        valid = 11 - resto;
+    }
+
+    return valid;
 }
+
+
 int main(int argc, char *argv[]) {
 	int num1, num2, num3, num4, num5, num6, num7, num8, num9, num10, num11, validar1, validar2;
 	
 
-	printf("Digite um cpf: ");
+	printf("Digite um CPF: ");
 	scanf("%d %d %d %d %d %d %d %d %d %d %d", &num1, &num2, &num3, &num4, &num5, &num6, &num7, &num8, &num9, &num10, &num11);
 	
 	printf("O cpf eh: %d%d%d.%d%d%d.%d%d%d-%d%d\n", num1, num2, num3, num4, num5, num6, num7, num8, num9, num10, num11);
