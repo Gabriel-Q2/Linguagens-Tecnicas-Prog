6fc70b61efdd198bd9695bf2c41f694fa2f80471
Gabriel Q. <quintanilhagabriel01@hotmail.com>
2026-09-15 08:45:27 -0300

    Update cpf.c

--- exercicios - C/cpf.c
+++ exercicios - C/cpf.c
@@ -1,81 +1,47 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int validaPrimeiro(int num1, int num2, int num3, int num4, int num5,
-                   int num6, int num7, int num8, int num9) {
-    int soma, resto, valid;
-
-    soma = num1 * 10 +
-           num2 * 9 +
-           num3 * 8 +
-           num4 * 7 +
-           num5 * 6 +
-           num6 * 5 +
-           num7 * 4 +
-           num8 * 3 +
-           num9 * 2;
-
-    resto = soma % 11;
-	
-	if (resto == 10) resto = 0;
-    if (resto < 2) {
-        valid = 0;
-    } else {
-        valid = 11 - resto;
-    }
-
-    return valid;
+int multDigito(int digito, int valor) {
+    return digito * valor;
 }
 
-int validaSegundo(int num1, int num2, int num3, int num4, int num5,
-                  int num6, int num7, int num8, int num9, int num10) {
-    int soma, resto, valid;
+int main() {
+    int num1, num2, num3, num4, num5, num6, num7, num8, num9, num10, num11, soma1, soma2, v10, v11;
 
-    soma = num1 * 11 +
-           num2 * 10 +
-           num3 * 9 +
-           num4 * 8 +
-           num5 * 7 +
-           num6 * 6 +
-           num7 * 5 +
-           num8 * 4 +
-           num9 * 3 +
-           num10 * 2;
+    printf("Digite o seu CPF (Sem pontuacao): ");
 
-    resto = soma % 11;
-	
-	if (resto == 10) resto = 0;
-    if (resto < 2) {
-        valid = 0;
+    scanf("%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d", &num1, &num2, &num3, &num4, &num5, &num6, &num7, &num8, &num9, &num10, &num11);
+    printf("O cpf eh: %d%d%d.%d%d%d.%d%d%d-%d%d\n", num1, num2, num3, num4, num5, num6, num7, num8, num9, num10, num11);
+
+    soma1 = multDigito(num1, 10)
+          + multDigito(num2, 9)
+          + multDigito(num3, 8)
+          + multDigito(num4, 7)
+          + multDigito(num5, 6)
+          + multDigito(num6, 5)
+          + multDigito(num7, 4)
+          + multDigito(num8, 3)
+          + multDigito(num9, 2);
+
+    v10 = (soma1 * 10) % 11;
+
+    soma2 = multDigito(num1, 11)
+          + multDigito(num2, 10)
+          + multDigito(num3, 9)
+          + multDigito(num4, 8)
+          + multDigito(num5, 7)
+          + multDigito(num6, 6)
+          + multDigito(num7, 5)
+          + multDigito(num8, 4)
+          + multDigito(num9, 3)
+          + multDigito(num10, 2);
+
+    v11 = (soma2 * 10) % 11;
+
+    if (v10 == num10 && v11 == num11) {
+        printf("CPF valido!\n");
     } else {
-        valid = 11 - resto;
+        printf("CPF INvalido!\n");
     }
 
-    return valid;
-}
-
-
-int main(int argc, char *argv[]) {
-	int num1, num2, num3, num4, num5, num6, num7, num8, num9, num10, num11, validar1, validar2;
-	
-
-	printf("Digite um CPF: ");
-	scanf("%d %d %d %d %d %d %d %d %d %d %d", &num1, &num2, &num3, &num4, &num5, &num6, &num7, &num8, &num9, &num10, &num11);
-	
-	printf("O cpf eh: %d%d%d.%d%d%d.%d%d%d-%d%d\n", num1, num2, num3, num4, num5, num6, num7, num8, num9, num10, num11);
-	
-
-	validar1 = validaPrimeiro(num1, num2, num3, num4, num5, num6, num7, num8, num9);
-	printf("O numero valid1 eh: %d\n", validar1);
-	
-	validar2 = validaSegundo(num1, num2, num3, num4, num5, num6, num7, num8, num9, validar1);
-	printf("O numero valid2 eh: %d\n", validar2);
-	
-	if (validar1 == num10 && validar2 == num11) {
-		printf("CPF valido");
-	} else {
-		printf("CPF INvalido");
-	}
-	
-	return 0;
+    return 0;
 }
